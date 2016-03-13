06b969a48d7586dc0a2794fbcef65eb97aa07d8e
benjapg <benjamin.paul.graham@okstate.edu>
2016-03-12 18:20:49 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -35,18 +35,18 @@ var config = {
             'Hi, sexy!'
         ]
     },
-    calendar: {
-        maximumEntries: 10, // Total Maximum Entries
-		displaySymbol: true,
-		defaultSymbol: 'calendar', // Fontawsome Symbol see http://fontawesome.io/cheatsheet/
-        urls: [
+    //calendar: {
+        //maximumEntries: 10, // Total Maximum Entries
+		//displaySymbol: true,
+		//defaultSymbol: 'calendar', // Fontawsome Symbol see http://fontawesome.io/cheatsheet/
+        //urls: [
 		{
-			symbol: 'calendar-plus-o', 
-			url: 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M'
+			//symbol: 'calendar-plus-o', 
+			//url: 'https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M'
 		},
 		{
-			symbol: 'soccer-ball-o',
-			url: 'https://www.google.com/calendar/ical/akvbisn5iha43idv0ktdalnor4%40group.calendar.google.com/public/basic.ics',
+			//symbol: 'soccer-ball-o',
+			//url: 'https://www.google.com/calendar/ical/akvbisn5iha43idv0ktdalnor4%40group.calendar.google.com/public/basic.ics',
 		},
 		// {
 			// symbol: 'mars',
